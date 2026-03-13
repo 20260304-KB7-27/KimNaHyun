4488b29619c3aa6492f8d9ca15f27f62a03cba95
nhkim06 <nhkim7788@gmail.com>
2026-03-13 17:36:09 +0900

    update : edit code

--- 02_Node/02_advanced/01_Node/00_async/async-2.js
+++ 02_Node/02_advanced/01_Node/00_async/async-2.js
@@ -1,4 +1,4 @@
-let likePizza = false;
+let likePizza = true;
 const pizza = new Promise((resolve, reject) => {
   if (likePizza) {
     resolve('피자를 주문합니다. ');
@@ -7,20 +7,8 @@ const pizza = new Promise((resolve, reject) => {
   }
 });
 
-const orderPizza = new Promise((resolve, reject) => {
-  if (likePizza) {
-    resolve('피자를 주문했습니다. ');
-  } else {
-    reject('피자를 주문하지 않습니다. ');
-  }
-});
-
 pizza
   .then((result) => {
     console.log(result);
-    return orderPizza;
-  })
-  .then((order) => {
-    console.log(order);
   })
   .catch((err) => console.log(err));
