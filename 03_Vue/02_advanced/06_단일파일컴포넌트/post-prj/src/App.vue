931fbcbe29fbbe4bf9f715615c9ed1dd54e09eac
nhkim06 <nhkim7788@gmail.com>
2026-03-24 18:50:49 +0900

    save : uptodate (have to finish distributing files of post prj)

--- 03_Vue/02_advanced/06_단일파일컴포넌트/post-prj/src/App.vue
+++ 03_Vue/02_advanced/06_단일파일컴포넌트/post-prj/src/App.vue
@@ -1,11 +1,96 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>📌 게시판 목록 v2</h1>
+  <InputPost />
 </template>
 
-<style scoped></style>
+<script setup>
+import InputPost from './components/InputPost.vue';
+import data from './data/data.json';
+
+const { ref } = Vue;
+let ts = new Date().getTime();
+const posts = ref(data);
+
+const selectedPosts = ref([]);
+
+function selectPost(post) {
+  if (!selectedPosts.value.includes(post)) {
+    selectedPosts.value.push(post);
+  } else {
+    selectedPosts.value = selectedPosts.value.filter((p) => p.no !== post.no);
+  }
+  console.log(selectedPosts);
+}
+
+function closePost(post) {
+  if (selectedPosts.value.includes(post)) {
+    selectedPosts.value = selectedPosts.value.filter((p) => p.no !== post.no);
+  }
+  console.log(selectedPosts);
+}
+
+const nextNo = ref(Math.max(...data.map((p) => p.no), 0) + 1);
+const newPost = ref({
+  title: null,
+  content: null,
+});
+
+function addPost() {
+  if (!newPost.value.title || !newPost.value.content) {
+    alert('제목과 내용은 빈칸일 수 없습니다.');
+    return;
+  }
+
+  posts.value.push({
+    no: nextNo.value,
+    ...newPost.value,
+  });
+
+  nextNo.value++;
+  newPost.value.title = null;
+  newPost.value.content = null;
+}
+
+function deletePost(no) {
+  posts.value = posts.value.filter((p) => p.no !== no);
+  selectedPosts.value = selectedPosts.value.filter((p) => p.no !== no);
+}
+
+const editingPost = ref({
+  no: null,
+  title: null,
+  content: null,
+});
+
+function startEdit(post) {
+  editingPost.value = { ...post };
+}
+
+function saveEdit() {
+  if (!editingPost.value.title || !editingPost.value.content) {
+    alert('내용을 입력해주세요.');
+    return;
+  }
+
+  const index = posts.value.findIndex((p) => p.no === editingPost.value.no);
+
+  if (index !== -1) {
+    posts.value[index] = { ...editingPost.value };
+    const sIndex = selectedPosts.value.findIndex(
+      (p) => p.no === editingPost.value.no,
+    );
+    if (sIndex !== -1) {
+      selectedPosts.value[sIndex] = { ...editingPost.value };
+    }
+  }
+  cancelEdit();
+}
+
+function cancelEdit() {
+  editingPost.value = {
+    no: null,
+    title: null,
+    content: null,
+  };
+}
+</script>
---
+++ 03_Vue/02_advanced/06_단일파일컴포넌트/post-prj/src/components/InputPost.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <input type="text" v-model="newPost.title" />
+    <input type="text" v-model="newPost.content" />
+    <button @click="addPost()">추가</button>
+  </div>
+</template>
+
+<script setup>
+import { newPost, addPost } from './repository/dataManager.js';
+</script>
