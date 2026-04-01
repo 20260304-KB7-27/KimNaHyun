8fb4b9d812ffe8af170ee4d896e3366355d3499c
nhkim06 <nhkim7788@gmail.com>
2026-04-01 16:48:54 +0900

    update : Vue 09 Router

---
+++ 00_assignment/ass01/router-test/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView, RouterLink } from 'vue-router';
+import Header from './pages/Header.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+</script>
+
+<template>
+  <div class="container">
+    <Header></Header>
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 10px;
+}
+</style>
