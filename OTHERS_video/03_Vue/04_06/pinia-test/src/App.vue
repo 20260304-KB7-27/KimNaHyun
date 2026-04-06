425a5149e2b0b9e8826a3cc08bc63cc13275aa62
nhkim06 <nhkim7788@gmail.com>
2026-04-06 12:38:24 +0900

    update : vLecture

---
+++ OTHERS_video/03_Vue/04_06/pinia-test/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useCounterStore } from './stores/counter.js';
+
+import { computed } from 'vue';
+const { increment } = useCounterStore();
+const store = useCounterStore();
+
+const doubleCount = computed(() => store.doubleCount);
+
+// const store = useCounterStore();
+// console.log(store);
+</script>
+
+<template>
+  count : {{ store.count }}<br />
+  double count : {{ store.doubleCount }}<br />
+  <button @click="store.increment">Increment</button>
+</template>
+
+<style scoped></style>
