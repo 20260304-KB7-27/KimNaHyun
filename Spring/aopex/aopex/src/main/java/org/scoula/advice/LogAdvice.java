5c4d33305a903f111ae0f2d318835e790308e3eb
nhkim06 <nhkim7788@gmail.com>
2026-06-24 14:49:48 +0900

    temp

---
+++ Spring/aopex/aopex/src/main/java/org/scoula/advice/LogAdvice.java
@@ -0,0 +1,56 @@
+package org.scoula.advice;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+// Aspect : 공통 관심사(로깅)를 모아놓은 모듈
+@Aspect
+@Component
+@Log4j2
+public class LogAdvice {
+
+    // Advice : Target에다가 적용시킬 동작들
+
+    // Before Advice : 대상 메서드(Target) 실행 직전에 동작
+
+    // execution : Target을 지정하는 방법
+    // execution([반환타입] [클래스풀네임].[메소드명])
+    @Before(value = "execution(* org.scoula.sample.service.SampleService*.doAdd(String, String))"
+            + "&&args(str1, str2)", argNames = "str1, str2"
+    )
+
+    public void logBeforeWithParam(String str1, String str2){
+        log.info("🍟 Before Advice 동작합니다...!");
+        log.info("str1 : {}", str1);
+        log.info("str2 : {}", str2);
+    }
+
+
+    // *(..) : 해당 클래스의 모든 메서드, 모든 파라미터 적용
+    @AfterThrowing(value = "execution(* org.scoula.sample.service.SampleService*.*(..))"
+            , throwing = "exception"
+    )
+
+    public void logAfterThrowing(Exception exception){
+        log.info("🍟 AfterThrowing 동작합니다...!");
+        log.info("exception : {}", exception.getMessage());
+    }
+
+
+
+    @Around(value = "execution(* org.scoula.sample.service.SampleService*.*(..))")
+
+    public void aroundTest(Exception exception){
+        log.info("🍟 Around 동작합니다...!");
+
+        // 실제 메소드 호출 필요
+    }
+
+
+
+}
